046bbcb386754035a6662d0535e69a46dc240198
Andreas Deil <andreas.deil@itp-mail.com>
2013-05-29 08:35:28 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oinfo::Application.config.secret_token = '08fbeaf3946e005209525dd442e63604c96f15a5882c52989b5b95f7cc7eefad37216a9088c257cec78d28a40116ab4caf6c7ab58c9965361178dbda8eec2ccb'
